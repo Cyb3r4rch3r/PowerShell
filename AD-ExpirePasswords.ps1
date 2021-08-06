656bbbfe0994c27294e81302ba48cf1383255853
Cyb3r4rch3r <Cyb3r4rch3r@gmail.com>
2021-08-06 10:07:51 -0400

    Create AD-ExpirePasswords.ps1
    Force Password Change for All Users Who Haven't Changed Their Passwords in 'N' Number of Days

---
+++ AD-ExpirePasswords.ps1
@@ -0,0 +1,68 @@
+<#
+.SYNOPSIS
+    Force Password Change for All Users Who Haven't Changed Their Passwords in 'N' Number of Days
+.EXAMPLE
+    ./AD-ExpirePasswords.ps1 
+.INPUTS
+    None
+.COMPONENT
+    PowerShell, Active Directory Module
+.ROLE
+    Sufficient Rights to Query AD
+.FUNCTIONALITY
+    Force Password Change for All Users Who Haven't Changed Their Passwords in 'N' Number of Days
+#>
+
+$username = $env:username
+
+$file = "C:\Users\$username\Documents\users.txt"
+
+Function Gather{
+    $days = Read-Host -Prompt "Enter number of days you wish to exclude"
+    $date = (get-date).adddays(-$days)
+    Get-aduser -filter {(enabled -eq $true) -and (passwordlastset -lt $date) -and (name -notlike "*$")} | Select-Object samaccountname | Out-File $file
+    }
+    
+$Answer = Read-Host -Prompt "Do you already have a file to work from? (Y|N)"
+
+if ($Answer -like 'y'){
+    Gather
+    }
+
+#Create Open File Dialogue to get path for Users.txt file
+Function OpenFile{
+    Add-Type -AssemblyName System.Windows.Forms
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ InitialDirectory = [Environment]::GetFolderPath('MyDocuments') ; Filter = 'Documents (*.txt)|*.txt'}
+    $null = $FileBrowser.ShowDialog()
+    $path = $FileBrowser.FileName
+    Return $path
+  }
+
+Function GetInfo {
+    $count = Get-Content $file
+    
+    $count.count
+    
+    $chunk = Read-Host -Prompt "How many users do you want to apply this to? (Numerical values only)"
+    
+    $forceChange = $count | Select-Object -first $chunk
+    
+    foreach ($account in $forceChange){set-aduser $account -changepasswordatlogon $true -whatif}
+    
+    foreach ($user in $forceChange){write-output $user; Set-Content -Path $file -value(Get-Content $file | Select-String -Pattern $user -NotMatch)}
+    
+    $count = Get-Content $file
+    
+    $count.Count
+}
+
+if ((Test-Path $file) -eq $true){
+  
+}
+Else {
+    Write-Error "File does not Exist. Please choose the correct path."
+    $file = OpenFile
+
+    GetInfo
+
+}
