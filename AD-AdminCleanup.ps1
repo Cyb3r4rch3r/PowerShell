cb09a2c51d227604f415ee76b6bbfeff9318c351
Cyb3r4rch3r <Cyb3r4rch3r@gmail.com>
2021-08-10 16:54:10 -0400

    Create AD-AdminCleanup.ps1

---
+++ AD-AdminCleanup.ps1
@@ -0,0 +1,66 @@
+<#
+.SYNOPSIS
+    Change users password, remove the PasswordNeverExpires flag, and set ChangePasswordAtLogon to force a password change for all Admin accounts that haven't logged in for 90 days or more
+.DESCRIPTION
+    Change users password, remove the PasswordNeverExpires flag, and set ChangePasswordAtLogon to force a password change for all Admin accounts that haven't logged in for 90 days or more
+.INPUTS
+    Username is required
+.COMPONENT
+    PowerShell Active Directory Module
+.ROLE
+    Sufficient AD rights to manage user objects
+.FUNCTIONALITY
+    Change users password, remove the PasswordNeverExpires flag, and set ChangePasswordAtLogon to force a password change for all Admin accounts that haven't logged in for 90 days or more
+#>
+
+
+$date = (Get-Date).AddDays(-90) 
+
+$admins = Get-ADUser -Filter {(adminCount -like "1") -and (enabled -eq $true)} | Where-Object {$_.lastlogondate -le $date}
+
+function Pass { 
+
+#Generate Password
+
+Add-Type -AssemblyName 'System.Web'
+$minLength = 90 ## characters
+$maxLength = 120 ## characters
+$length = Get-Random -Minimum $minLength -Maximum $maxLength
+$nonAlphaChars = 5
+$password = [System.Web.Security.Membership]::GeneratePassword($length, $nonAlphaChars)
+
+Write-Output "The new password is" $password
+
+$password2 = convertto-securestring $password -AsPlainText -Force
+
+Return $password2
+
+}
+
+
+Function Cleanup {
+    foreach ($uName in $admins) {
+        Set-ADAccountPassword -Identity $uName.samaccountname -NewPassword (Pass)
+        Set-ADUser $uName.samaccountname -PasswordNeverExpires $false
+        Set-ADUser $uName.samaccountname -ChangePasswordAtLogon $true
+        
+        $grpMemberships = Get-ADPrincipalGroupMembership $uName.samaccountname  | where-object {$_.name -ne "Domain Users"} | Select-Object name
+
+		foreach ($grpMembership in $grpMemberships.name) {
+			Write-Output "$grpMembership found!"
+			get-adgroup -filter "name -like '$grpMembership'" | Remove-ADGroupMember -members $uName.samaccountname -Confirm:$false
+			Write-Output "Removing $($uName.name) from: $grpMembership"
+			"$($uName.name) is a member of: $grpMembership" | out-file -filepath Disabled-Admin_Accounts-$(get-date -f yyy-MM-dd).txt -Append
+			Write-Output ""
+			}
+
+
+        #Disable user
+		Write-Output "Disabling $uName.name"
+		Disable-ADAccount -identity $uName.samaccountname
+
+        #Add Description
+		$day = Get-Date -Format g
+		Set-ADUser $uName.samaccountname -Description "Disabled by $admin on $day"
+    }
+}
